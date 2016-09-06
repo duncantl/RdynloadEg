65453cb08e6a376c57a5d9d1aad34ecd7e172236
Duncan Temple Lang <duncan@r-project.org>
2016-09-06 11:05:09 -0700

    added param names, try() to allow the code to fail

---
+++ README.md
@@ -0,0 +1,18 @@
+This directory contains some examples illustrating the effects of
+using different arguments to the dyn.load() function in R on how the
+symbols are loaded, resolved and shared.
+
+See [index.html](index.html) for an explanation.
+
+To build the examples, type
+```shell
+make
+```
+at the shell prompt.
+You can run the code in code.R.
+
+
+
+
+
+
--- code.R
+++ code.R
@@ -1,16 +1,16 @@
-dyn.load("T.so")   # Fails, missing symbol Rso
+dso = sprintf("%s%s", c("Central", "T", "R"), .Platform$dynlib.ext)
+if(!all(file.exists(dso)))
+   stop("You need to build all of the shared libraries. Use make at the shell prompt")
 
+try(dyn.load("T.so"))   # Fails, missing symbol Rso
 
-dyn.load("T.so",,FALSE) # Success, but don't call foo().
+dyn.load("T.so", , now = FALSE) # Success, but don't call foo().
 .C("selfReliant")
-#In selfReliant() (bar.c in T.so)
-#list()
-dyn.load("Central.so")
-dyn.load("T.so")  # Fails missing Rso
 
-dyn.load("Central.so",FALSE)
+dyn.load("Central.so")
+try(dyn.load("T.so"))  # Fails missing Rso
+
+dyn.load("Central.so", local = FALSE)
 dyn.load("T.so")
 .C("foo")
-#Second External foo()
-#In Rso.
-#list()
+
